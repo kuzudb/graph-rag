d806f237ebba86bbc39d9ed1238d0a0438bc3b92
prrao87 <prrao87@gmail.com>
2024-12-02 14:25:01 -0500

    Update dataset for similarity search example

--- src/03_movie_wiki/create_graph.py
+++ src/03_movie_wiki/create_graph.py
@@ -1,19 +1,31 @@
 """
 Create a graph in Kùzu
 """
-
+import os
 import shutil
 
 import kuzu
+import polars as pl
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
 
 shutil.rmtree("test_kuzudb", ignore_errors=True)
 db = kuzu.Database("test_kuzudb")
 conn = kuzu.Connection(db)
 
+OPENAI_CLIENT = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+def embed(query: str) -> list:
+    response = OPENAI_CLIENT.embeddings.create(model="text-embedding-3-small", input=query)
+    return response.data[0].embedding
+
+
 # Define schema
 conn.execute("""
     CREATE NODE TABLE IF NOT EXISTS Actor(name STRING, age INT64, PRIMARY KEY(name));
-    CREATE NODE TABLE IF NOT EXISTS Movie(title STRING, year INT64, PRIMARY KEY(title));
+    CREATE NODE TABLE IF NOT EXISTS Movie(title STRING, year INT64, summary STRING, PRIMARY KEY(title));
     CREATE NODE TABLE IF NOT EXISTS Director(name STRING, age INT64, PRIMARY KEY(name));
     CREATE NODE TABLE IF NOT EXISTS Character(name STRING, description STRING, PRIMARY KEY(name));
     CREATE NODE TABLE IF NOT EXISTS Writer(name STRING, age INT64, PRIMARY KEY(name));
@@ -45,7 +57,37 @@ for table, file in files.items():
     conn.execute(f"COPY {table} FROM '{base_path}/{file}';")
 print("Finished ingesting data")
 
-# Query
+# Use an OpenAI embedding model to store a vector embedding of the "summary" property in the movie node
+df = pl.read_csv(
+    f"{base_path}/movie.csv",
+    has_header=False
+).rename({
+    "column_1": "title",
+    "column_2": "year",
+    "column_3": "summary"
+})
+df.head()
+final_df = (
+    df.with_columns(
+        pl.col("summary").map_elements(embed, return_dtype=pl.List(pl.Float32)).alias("vector")
+    )
+)
+final_df.head()
+
+# Add an embedding column to the `Movie` node table
+conn.execute("""
+    ALTER TABLE Movie ADD vector DOUBLE[1536];
+""")
+
+conn.execute(
+    """
+    LOAD FROM final_df
+    MERGE (m:Movie {title: title})
+    ON MATCH SET m.vector = vector
+    """
+)
+
+# Query the graph to check what we have
 print("---\nHere are the actors and the characters they played in Interstellar:")
 res = conn.execute(
     """
@@ -57,3 +99,18 @@ res = conn.execute(
 while res.has_next():
     data = res.get_next()
     print(f"{data[0]} -> {data[1]}")
+
+
+# Similarity search to find a movie whose summary embedding is closest to the query embedding
+query_item = "space opera sci-fi movie"
+query_vector = embed(query_item)
+res = conn.execute(
+    """
+    MATCH (m:Movie)<-[:WROTE]-(w:Writer)
+    WITH m, w, array_cosine_similarity(m.vector, $query_vector) AS similarity
+    RETURN m.title AS title, m.summary AS summary, COLLECT(w.name) AS writers LIMIT 3;
+    """,
+    parameters={"query_vector": query_vector}
+)
+result = res.get_as_pl()
+print(f"---\nMovies that are closest to the query '{query_item}':\n{result}")
